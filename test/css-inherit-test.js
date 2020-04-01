0b4e08b7be010ec974ca47966e936fda316112e4
Jean Honlet <jehon@users.noreply.github.com>
2020-04-01 21:40:21 +0200

    test: first mock test

---
+++ test/css-inherit-test.js
@@ -0,0 +1,8 @@
+
+describe('css-inherit', function () {
+	it('should', function () {
+		expect(true).toBeTruthy();
+	});
+
+
+});
---
+++ test/karma.conf.js
@@ -0,0 +1,48 @@
+/* eslint-env node */
+/* eslint no-console: off */
+
+const path = require('path');
+const fse = require('fs-extra');
+const root = path.dirname(__dirname);
+fse.emptyDirSync(path.join(root, 'tmp'));
+
+// https://blog.cepharum.de/en/post/natively-unit-testing-es6-modules-in-browser-including-coverage.html
+
+module.exports = function (config) {
+	const configuration = {
+		basePath: root,
+
+		frameworks: [
+			'jasmine',
+			'jasmine-html'
+		],
+
+		reporters: [
+			'progress',
+			'coverage-istanbul'
+		],
+
+		files: [
+			{ pattern: 'css-inherit.js' },
+			{ pattern: 'test/*-test.js' },
+			{ pattern: '**/*', included: false, watched: false },
+		],
+
+		autoWatch: true,
+
+		browsers: [
+			'ChromeHeadless'
+		],
+
+		preprocessors: {
+			'css-inherit.js': ['karma-coverage-istanbul-instrumenter'],
+		},
+
+		coverageIstanbulReporter: {
+			reports: ['html', 'text'],
+			dir: path.join(root, 'tmp/coverage'),
+		}
+	};
+
+	config.set(configuration);
+};
