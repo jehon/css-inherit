552363abfaa1bba41144064cc5384c6f5fc70939
Jean Honlet <jehon@users.noreply.github.com>
2023-03-10 09:17:43 +0000

    chore: refactor build environment

--- jehon-css-inherit.js
+++ jehon-css-inherit.js
@@ -1,31 +1,34 @@
-
 class CssInherit extends HTMLElement {
-	connectedCallback() {
-		this.update();
-	}
+  connectedCallback() {
+    this.update();
+  }
 
-	update() {
-		// Reset
-		this.innerHTML = '';
+  update() {
+    // Reset
+    this.innerHTML = "";
 
-		// Get the enclosing shadowRoot
-		const firstShadowRoot = this.getRootNode();
+    // Get the enclosing shadowRoot
+    const firstShadowRoot = this.getRootNode();
 
-		// Enclosing parent
-		let enclosingParent = firstShadowRoot.host;
-		if (!enclosingParent) {
-			return;
-		}
-		// The parent root (shadowRoot or document, whatever)
-		const parentShadowRoot = enclosingParent.getRootNode();
+    // Enclosing parent
+    let enclosingParent = firstShadowRoot.host;
+    if (!enclosingParent) {
+      return;
+    }
+    // The parent root (shadowRoot or document, whatever)
+    const parentShadowRoot = enclosingParent.getRootNode();
 
-		// Let's take all style / link that are not 'local'(attribute)
-		parentShadowRoot.querySelectorAll('style:not([jehon-css-inherit-local]), link:not([jehon-css-inherit-local])').forEach(el => {
-			const node = el.cloneNode(true);
-			// Add this inside our innerHTML
-			this.insertAdjacentElement('beforeend', node);
-		});
-	}
+    // Let's take all style / link that are not 'local'(attribute)
+    parentShadowRoot
+      .querySelectorAll(
+        "style:not([jehon-css-inherit-local]), link:not([jehon-css-inherit-local])"
+      )
+      .forEach((el) => {
+        const node = el.cloneNode(true);
+        // Add this inside our innerHTML
+        this.insertAdjacentElement("beforeend", node);
+      });
+  }
 }
 
-customElements.define('jehon-css-inherit', CssInherit);
+customElements.define("jehon-css-inherit", CssInherit);
--- test/css-inherit-test.js
+++ test/css-inherit-test.js
@@ -1,50 +1,47 @@
-
 /* global withHtml */
 
 class XSauron extends HTMLElement {
-	// One Ring to *bring them all* and in the darkness bind them
-	// In the Land of Mordor where the *Shadows* lie
+  // One Ring to *bring them all* and in the darkness bind them
+  // In the Land of Mordor where the *Shadows* lie
 
-	constructor(...args) {
-		super(...args);
-		this.attachShadow({ mode: 'open' });
-		this.shadowRoot.innerHTML = '';
-	}
+  constructor(...args) {
+    super(...args);
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = "";
+  }
 
-	connectedCallback() {
-		// Move all into the Shadows
-		if (this.innerHTML > '') {
-			this.shadowRoot.innerHTML = this.innerHTML;
-			this.innerHTML = '';
-		}
-	}
+  connectedCallback() {
+    // Move all into the Shadows
+    if (this.innerHTML > "") {
+      this.shadowRoot.innerHTML = this.innerHTML;
+      this.innerHTML = "";
+    }
+  }
 
-	e(sel) {
-		const e = this.shadowRoot.querySelectorAll(sel);
-		if (e.length != 1) {
-			throw `Non-unique result for ${sel}: ${e.length}`;
-		}
-		return e[0];
-	}
+  e(sel) {
+    const e = this.shadowRoot.querySelectorAll(sel);
+    if (e.length != 1) {
+      throw `Non-unique result for ${sel}: ${e.length}`;
+    }
+    return e[0];
+  }
 }
 
-customElements.define('x-sauron', XSauron);
+customElements.define("x-sauron", XSauron);
 
 function c(el) {
-	return window.getComputedStyle(el);
+  return window.getComputedStyle(el);
 }
 
-function sauron(element) {
-	return element.querySelector('x-sauron');
-}
+describe("jehon-css-inherit", function () {
+  it("trivial", function () {
+    expect(true).toBeTruthy();
+  });
 
-describe('jehon-css-inherit', function () {
-	it('trivial', function () {
-		expect(true).toBeTruthy();
-	});
-
-	withHtml({
-		title: 'test', html: `
+  withHtml(
+    {
+      title: "test",
+      html: `
 		<div>
 		<link rel="stylesheet" type="text/css" href='/base/test/test-local.css' jehon-css-inherit-local />
 		<link rel="stylesheet" type="text/css" href='/base/test/test.css' />
@@ -66,41 +63,52 @@ describe('jehon-css-inherit', function () {
 				<span class='u'>inside test2</span>
 			</x-sauron>
 		</div>
-	`}, function (element) {
-		it('trivial', function () {
-			expect(true).toBeTruthy();
-		});
+	`,
+    },
+    function (element) {
+      it("trivial", function () {
+        expect(true).toBeTruthy();
+      });
 
-		it('should inherit', function (done) {
-			setTimeout(() => {
-				const sauron = element().querySelector('x-sauron');
-				expect(sauron.e('div')).not.toBeNull();
-				expect(sauron.e('span')).not.toBeNull();
+      it("should inherit", function (done) {
+        setTimeout(() => {
+          const sauron = element().querySelector("x-sauron");
+          expect(sauron.e("div")).not.toBeNull();
+          expect(sauron.e("span")).not.toBeNull();
 
-				// On root element
-				expect(c(element().querySelector('div')).backgroundColor).toBe('rgb(255, 0, 0)');
-				expect(c(element().querySelector('span')).backgroundColor).toBe('rgb(0, 128, 0)');
+          // On root element
+          expect(c(element().querySelector("div")).backgroundColor).toBe(
+            "rgb(255, 0, 0)"
+          );
+          expect(c(element().querySelector("span")).backgroundColor).toBe(
+            "rgb(0, 128, 0)"
+          );
 
-				// Inside shadowRoot
-				expect(c(sauron.e('div')).backgroundColor).toBe('rgb(255, 0, 0)');
-				expect(c(sauron.e('span')).backgroundColor).toBe('rgb(0, 128, 0)');
-				done();
-			}, 100);
-		});
+          // Inside shadowRoot
+          expect(c(sauron.e("div")).backgroundColor).toBe("rgb(255, 0, 0)");
+          expect(c(sauron.e("span")).backgroundColor).toBe("rgb(0, 128, 0)");
+          done();
+        }, 100);
+      });
 
-		it('should not inherit tagged "local"', function (done) {
-			setTimeout(() => {
-				const sauron = element().querySelector('x-sauron');
+      it('should not inherit tagged "local"', function (done) {
+        setTimeout(() => {
+          const sauron = element().querySelector("x-sauron");
 
-				// On root element
-				expect(c(element().querySelector('div')).color).toBe('rgb(0, 128, 0)');
-				expect(c(element().querySelector('span')).color).toBe('rgb(138, 43, 226)');
+          // On root element
+          expect(c(element().querySelector("div")).color).toBe(
+            "rgb(0, 128, 0)"
+          );
+          expect(c(element().querySelector("span")).color).toBe(
+            "rgb(138, 43, 226)"
+          );
 
-				// Inside shadowRoot: not inherited
-				expect(c(sauron.e('div')).color).toBe('rgb(0, 0, 0)');
-				expect(c(sauron.e('span')).color).toBe('rgb(0, 0, 0)');
-				done();
-			}, 100);
-		});
-	});
+          // Inside shadowRoot: not inherited
+          expect(c(sauron.e("div")).color).toBe("rgb(0, 0, 0)");
+          expect(c(sauron.e("span")).color).toBe("rgb(0, 0, 0)");
+          done();
+        }, 100);
+      });
+    }
+  );
 });
